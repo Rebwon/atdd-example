3258ae94314e07a8fbaa4b26a02862f12854c2f1
Rebwon <msolo021015@naver.com>
2020-06-02 16:47:54 +0900

    move greet method to service and refactor test code

--- src/main/java/com/rebwon/atddexample/employee/GreetingService.java
+++ src/main/java/com/rebwon/atddexample/employee/GreetingService.java
@@ -1,11 +1,24 @@
 package com.rebwon.atddexample.employee;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class GreetingService {
+	private final EmployeeRepository repository;
+
+	public GreetingService(EmployeeRepository repository) {
+		this.repository = repository;
+	}
 
 	public String greet(String lastName) {
-		return null;
+		Optional<Employee> employee =
+			repository.findByLastName(lastName);
+		return employee
+			.map(e -> String.format("Hello %s %s!", e.getFirstName(), e.getLastName()))
+			.orElse("Who is this " + lastName + " you're talking about?");
 	}
 }
--- src/test/java/com/rebwon/atddexample/employee/GreetingServiceTest.java
+++ src/test/java/com/rebwon/atddexample/employee/GreetingServiceTest.java
@@ -22,30 +22,24 @@ class GreetingServiceTest {
 
 	@BeforeEach
 	void setUp() {
-		service = new GreetingService();
-		given(repository.findByLastName(nonExistingLastName))
-			.willReturn(Optional.empty());
-		given(repository.findByLastName(existingLastName))
-			.willReturn(Optional.of(new Employee(lastName, firstName)));
+		service = new GreetingService(repository);
 	}
 
 	@Test
 	void greet_with_nonExisting_last_name_should_return_default_message() {
-		/*String nonExistingLastName = "nonExistingLastName";
+		given(repository.findByLastName(nonExistingLastName))
+			.willReturn(Optional.empty());
+
 		String msg = service.greet(nonExistingLastName);
-		assertThat(msg).isEqualTo("Who is this " + nonExistingLastName + " you're talking about?");*/
-
-		String msg = greet(this.nonExistingLastName);
-		assertThat(msg).isEqualTo("Who is this " + this.nonExistingLastName + " you're talking about?");
-
-		String msg1 = greet(existingLastName);
-		assertThat(msg1).isEqualTo(String.format("Hello %s %s!", firstName, this.lastName));
+		assertThat(msg).isEqualTo("Who is this " + nonExistingLastName + " you're talking about?");
 	}
 
-	private String greet(String lastName) {
-		Optional<Employee> employee = repository.findByLastName(lastName);
-		return employee
-			.map(e -> String.format("Hello %s %s!", e.getFirstName(), e.getLastName()))
-			.orElse("Who is this " + lastName + " you're talking about?");
+	@Test
+	void greet_with_existing_last_name_should_return_hello_message_with_appropriate_names() {
+		given(repository.findByLastName(existingLastName))
+			.willReturn(Optional.of(new Employee(lastName, firstName)));
+
+		String msg1 = service.greet(existingLastName);
+		assertThat(msg1).isEqualTo(String.format("Hello %s %s!", firstName, lastName));
 	}
 }
